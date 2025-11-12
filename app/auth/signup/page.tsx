7ca15833f2fa48d6068e1ec358369a5ab33e0066
drikara <ouattara17drissa@gmail.com>
2025-11-12 15:25:14 +0000

    suppression du role sur le frontend

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -123,7 +123,7 @@ const handleSubmit = async (e: React.FormEvent) => {
               />
             </div>
 
-            <div>
+            {/* <div>
               <label htmlFor="role" className="block text-sm font-medium text-gray-700">
                 Rôle
               </label>
@@ -135,7 +135,7 @@ const handleSubmit = async (e: React.FormEvent) => {
                 className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-orange-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm rounded-md"
               >
                 <option value="JURY">Membre du Jury</option>
-                
+                <option value="WFM">Workforce Manager (Admin)</option>
               </select>
               <p className="mt-1 text-sm text-orange-500">
                 {formData.role === 'WFM' 
@@ -143,7 +143,7 @@ const handleSubmit = async (e: React.FormEvent) => {
                   : 'Accès limité aux évaluations face-à-face'
                 }
               </p>
-            </div>
+            </div> */}
 
             <div>
               <label htmlFor="password" className="block text-sm font-medium text-gray-700">

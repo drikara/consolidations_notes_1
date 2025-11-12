86f8994f4626fc3624ae34224d8e5a883cbe0f50
drikara <ouattara17drissa@gmail.com>
2025-11-12 09:58:53 +0000

    resolution des differents endpoints

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,9 +1,1 @@
-// app/api/auth/[...all]/route.ts
-import { auth } from "@/lib/auth"
-import { toNextJsHandler } from "better-auth/next-js"
-
-export const { GET, POST } = toNextJsHandler(auth)
-
-// ⭐ IMPORTANT: Configurez les headers
-export const runtime = "nodejs"
-export const dynamic = "force-dynamic"
+ .

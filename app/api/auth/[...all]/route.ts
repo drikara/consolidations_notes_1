93a746f0d586206695169d8d0729594051484933
drikara <ouattara17drissa@gmail.com>
2025-11-12 10:14:49 +0000

    ajout du fichier route.ts dans app/api/auth

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,1 +1,8 @@
- .
+// app/api/auth/[...all]/route.ts
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
+
+export const { GET, POST } = toNextJsHandler(auth);
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
